213029f9c299b1aba5b90a9db170c4c19ced2290
Sudhan <bhattaraiavinav400@gmail.com>
2023-02-21 21:31:03 +0545

    included the navbar

---
+++ weave-client/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import icon from '../assets/user-solid.svg'
+export default function Navbar(){
+    return(
+        <header className="navBar">
+            <div className="nav-elements">
+                <h1>Weave</h1>
+                {/* <div className='searchTop'><input type="text" placeholder='Search'/></div> */}
+                <ul>
+                    <li>Explore</li>
+                    <li>My Projects</li>
+                    <li>Create</li>
+                    <li><i className="fa fa-user"></i></li>
+                </ul>
+            </div>
+        </header>
+    )
+}
