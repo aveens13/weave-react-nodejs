f126856ef9dce0e3702cfec9de787707727dab9c
Sudhan Bhattarai <bhattaraiavinav400@gmail.com>
2023-05-02 09:24:01 +0545

    backend updated for the projects and tasks

--- Router/projectroutes.js
+++ Router/projectroutes.js
@@ -10,4 +10,9 @@ router.post(
   fileUpload({ createParentPath: true }),
   projects.fileupload
 );
+router.get("/api/checkmail/:email", projects.checkEmail);
+router.get("/api/project/:projectId", projects.getIndividualProject);
+router.get("/api/tasks/:projectId", projects.getTasks);
+router.post("/api/task/:projectId", projects.addTask);
+router.post("/api/updatetask/:taskid", projects.updateTask);
 module.exports = router;
